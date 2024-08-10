213023b8311904cc55de94302011b9e6975e2cd0
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2024-08-10 20:42:37 +0300

    hero-up

--- src/js/hero.js
+++ src/js/hero.js
@@ -9,7 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("is-close");
     document.body.classList.toggle("no-scroll");
   }
 })();
